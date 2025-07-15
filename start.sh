826a159e4c8ba42dcc236d96a17eef3186fe4a59
Mehmet <mehmetakiforakci@gmail.com>
2025-07-15 04:25:24 +0300

    -

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
 
-# Soket dosyası için gerekli olan /run/php dizinini oluştur.
+# Soket dosyası için gerekli olan /run/php dizinini oluştur ve sahipliğini ata.
 mkdir -p /run/php
+chown -R www-data:www-data /run/php
 
 # PHP-FPM'i, projemizdeki özel yapılandırma dosyasını kullanarak başlat.
 php-fpm -y php-fpm.conf --daemonize
