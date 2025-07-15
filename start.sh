0ce265e3d02459fff174674590fb656495488bfb
Mehmet <mehmetakiforakci@gmail.com>
2025-07-15 04:19:11 +0300

    -

--- start.sh
+++ start.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
 
-# PHP-FPM (PHP motoru) servisini arka planda başlat.
-php-fpm &
+# Soket dosyası için gerekli olan /run/php dizinini oluştur.
+mkdir -p /run/php
 
-# Nginx'i ön planda başlat. Bu, Railway'in uygulamanın çalıştığını anlaması için gereklidir.
-# Nginx, /etc/nginx/conf.d/default.conf dosyasındaki yapılandırmamızı kullanacaktır.
+# PHP-FPM'i, projemizdeki özel yapılandırma dosyasını kullanarak başlat.
+php-fpm -y php-fpm.conf --daemonize
+
+# Nginx'i ön planda başlat.
 echo "Nginx başlatılıyor..."
 nginx -g "daemon off;" 
